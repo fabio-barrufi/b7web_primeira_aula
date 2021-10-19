25ac0478cdc25358d56f716ac062330aff0918fd
fabio-barrufi <fabio_bpagano@yahoo.com.br>
2021-10-18 22:51:14 -0300

    Primeira aula - Bonieky

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('Fábio');
+  const [age, setAge] = useState(22);
+  const [bg, setBg] = useState('#FF0000')
+
+  const [list, setList] = useState([
+    "O rato roeu a roupa do rei de roma.",
+    "aqui vai uma frase poética",
+    "a inspiração desse programador é pouca"
+  ]);
+
+  const handle20 = () => {
+    setAge(20);
+    setBg("#00FF00")
+  }
+
+  const handle30 = () => {
+    setAge(30);
+    setBg("#FF0000")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    < div style={{ backgroundColor: bg }}>
+      <Header name={name} age={age} />
+
+
+      {age === 20 && <button onClick={handle30}>Tenho 30 anos </button>
+      }
+
+
+      {age === 30 && <button onClick={handle20}>Tenho 20 anos</button>}
+
+      <hr />
+
+      <ul>
+        {list.map((frase, index) => (
+          <li key={index}>{frase}</li>
+        ))}
+      </ul>
+
+
+      <Footer />
+    </div >
+  )
 }
 
 export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <h1>Parte de baixo do projeto</h1>
+    </footer>
+  );
+}
